8b2806084b601bd909c692394dfdd8d3863e7578
reethu550 <reethuv550@gmail.com>
2021-05-27 17:15:53 +0530

    20th commit

--- script.js
+++ script.js
@@ -1,53 +1,37 @@
-let guessBox = document.getElementById("number-input");
-let check = document.getElementById("guess");
-let history = document.getElementById("history");
-const randomNumber = Math.floor(Math.random()*100+1);
-let count = 1;
-let finalResult = false;
-history.innerHTML = "";
+let body = document.getElementById("body");
+console.log(body)
 
-function GuessingGame() {
-    if (finalResult === false) {
-        computeWinner();
-        viewHistory();
-        return finalResult;
+let profileImg = document.getElementById("profile-img");
+
+let table = document.getElementById("table");
+console.log(table)
+
+let th = document.getElementById("th");
+console.log(th)
+
+let modeToggle = document.getElementById("mode-toggle");
+console.log(modeToggle)
+
+
+function changeMode() {
+    
+    if (modeToggle.innerText === "Light") {
+        modeToggle.innerText = "Dark";
+    }else {
+        modeToggle.innerText = "Light";
     }
-}   
 
-function viewHistory() {
-    let manualGuess = guessBox.value;
-    history.innerHTML +=  "You Guessed " + manualGuess;
+    body.classList.toggle("dark-body");
+    body.classList.toggle("light-body");
+    modeToggle.classList.toggle("light-toggle");
+    modeToggle.classList.toggle("dark-toggle");
+    profileImg.classList.toggle("light-profile-img");
+    profileImg.classList.toggle("dark-profile-img");
+    table.classList.toggle("dark-table");
+    table.classList.toggle("light-table");
+    th.classList.toggle("dark-toggle");
+    th.classList.toggle("light-toggle");
 
 }
 
-function computeWinner() {
-    let result = document.getElementById("result");
-    let guess = guessBox.value;
-
-    if (count < 5) {
-        if (randomNumber == guess) {
-            result.innerHTML = "<h1>You Won!</h1>";
-            finalResult = true;
-        } else if (randomNumber < guess) {
-            result.innerHTML = "<h1>You Too High!</h1>";
-            finalResult = false;
-        } else {
-            result.innerHTML = "<h1>You Too Low!</h1>";
-            finalResult = false;
-        }
-        count ++;
-    } else {
-        if (randomNumber == guess) {
-            result.innerHTML = "<h1>Great You Won!</h1>";
-            finalResult = true;
-        }
-        else {
-            result.innerHTML = "<h1> Oops You Lose!</h1>";
-            finalResult = true;
-        }
-    }
-}
-
-
-
-check.addEventListener("click", GuessingGame);
+modeToggle.addEventListener("click", changeMode);
